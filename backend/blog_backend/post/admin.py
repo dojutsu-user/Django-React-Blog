1e9897d3028f956c68ca7a843407146d1870049e
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-14 13:13:28 +0530

    Register the 'Post' model in admin.py

--- backend/blog_backend/post/admin.py
+++ backend/blog_backend/post/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'is_published']
+
+
+admin.site.register(Post, PostAdmin)
