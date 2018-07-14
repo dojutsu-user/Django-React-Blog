cb51770f8764d6820baaeac57a4c886f37b4c8d0
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-14 13:30:12 +0530

    Add functionality to generate unique slug after saving a 'post'

--- backend/blog_backend/post/models.py
+++ backend/blog_backend/post/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .utils import unique_slug_generator
+
 
 User = get_user_model()
 
@@ -11,13 +16,22 @@ class Post(models.Model):
     body = models.TextField()
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts', related_query_name='post')
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True, null=True)
     is_published = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     published_on = models.DateTimeField(null=True, blank=True)
+    last_edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
         indexes = [models.Index(fields=['slug'])]
+
+
+@receiver(post_save, sender=Post)
+def generate_unique_slug_for_posts(sender, instance, created, *args, **kwargs):
+
+    if created:
+        instance.slug = unique_slug_generator(instance)
+        instance.save()
