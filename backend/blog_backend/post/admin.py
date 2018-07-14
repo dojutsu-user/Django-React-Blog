213612bc369f39f8f8aa5f194acb3c7bd1d70d1a
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-14 14:28:54 +0530

    Add 'Comment' as StackedInline field in 'Post' Admin

--- backend/blog_backend/post/admin.py
+++ backend/blog_backend/post/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 
 from .models import Post
+from comment.models import Comment
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'is_published']
+
+    def post_comment_count(self, obj):
+        return Comment.objects.filter(post=obj).count()
+
+    post_comment_count.short_description = 'Total Comments'
+
+    inlines = [
+        CommentInline,
+    ]
+    list_display = ['title', 'author', 'is_published', 'post_comment_count']
 
 
 admin.site.register(Post, PostAdmin)
