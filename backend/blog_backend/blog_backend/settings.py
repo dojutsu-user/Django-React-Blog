f9fa1df5a12ee77c700940484389bf94b5b28491
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-14 13:18:04 +0530

    Update settings.py for Timezone and .gitignore

--- backend/blog_backend/blog_backend/settings.py
+++ backend/blog_backend/blog_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
