8285a7480859815c4ee038c45dca2b614dd95fd2
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-14 14:09:04 +0530

    Create 'Comment' Model

--- backend/blog_backend/blog_backend/settings.py
+++ backend/blog_backend/blog_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'post',
-    'rest_framework'
+    'rest_framework',
+    'comment',
 ]
 
 MIDDLEWARE = [
---
+++ backend/blog_backend/comment/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from post.models import Post
+
+
+class Comment(models.Model):
+    """Model For The Comments In The Blog Posts"""
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    website = models.URLField(blank=True, null=True)
+    body = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comment', related_query_name='comments')
+    is_displayed = models.BooleanField(default=True)
+    published_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Post - {self.post.title}, Body - {self.body}'
