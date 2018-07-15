e67e9a48caf0ec58dad84e1f9d6367a4c7b44961
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-15 14:35:42 +0530

    Install and set djang-cors-headers

--- backend/blog_backend/blog_backend/settings.py
+++ backend/blog_backend/blog_backend/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'post',
     'rest_framework',
     'comment',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'
+)
 
 ROOT_URLCONF = 'blog_backend.urls'
 
