2a6057ea1c45fc1df0eec3e89e007551e5f0aa20
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-15 22:33:05 +0530

    Add 'author_full_name' in serializer

--- backend/blog_backend/api/post/serializers.py
+++ backend/blog_backend/api/post/serializers.py
@@ -8,18 +8,20 @@ class PostListSerializer(serializers.ModelSerializer):
     """DRF Serializer Listing All The Blog Posts"""
 
     total_comments = serializers.IntegerField()
+    author_full_name = serializers.CharField()
 
     class Meta:
         model = Post
         fields = ['title', 'short_description',
-                  'total_comments', 'author', 'published_on']
+                  'total_comments', 'author_full_name', 'published_on']
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
     """DRF Serializer For Details Of The Blog Posts"""
 
     comments_list = CommentSerializer(many=True)
+    author_full_name = serializers.CharField()
 
     class Meta:
         model = Post
-        fields = ['title', 'body', 'author', 'published_on', 'comments_list']
+        fields = ['title', 'body', 'author_full_name', 'published_on', 'comments_list']
