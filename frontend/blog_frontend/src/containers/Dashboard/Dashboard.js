0347479a73315f8c1f43023b58a1614d97d67a73
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-20 17:56:38 +0530

    Setup Basic Dashboard CSS

--- frontend/blog_frontend/src/containers/Dashboard/Dashboard.js
+++ frontend/blog_frontend/src/containers/Dashboard/Dashboard.js
@@ -9,9 +9,23 @@ class Dashboard extends Component {
     render() {
         return (
             <Aux>
-                {/* {!this.props.isAuth ? <Redirect to="/" /> : null} */}
-                <div className={cssClass.Dashboard}>
-                    <h1>Hello World</h1>
+                <div className={cssClass.OuterWrapper}>
+                    {" "}
+                    <div class={cssClass.InnerWrapper}>
+                        <div className={cssClass.Container}>
+                            View Your Profile
+                        </div>
+                        <div className={cssClass.Container}>
+                            Edit Your Profile
+                        </div>
+                    </div>
+                    <div className={cssClass.InnerWrapper}>
+                        <div className={cssClass.Container}>Create A Post</div>
+                        <div className={cssClass.Container}>Your Posts</div>
+                    </div>
+                    <div className={cssClass.InnerWrapper}>
+                        <div className={cssClass.Container}>Admin Panel</div>
+                    </div>
                 </div>
             </Aux>
         );
