ea4dda413aeb47c2d58465d216c20d9faea635e7
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-26 12:20:36 +0530

    Add Authentication Classes To Admin Views

--- backend/blog_backend/api/admin/post/views.py
+++ backend/blog_backend/api/admin/post/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
-from rest_framework.authentication import TokenAuthentication
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .serializers import AdminPostDetailSerializer
 from api.post.serializers import PostListSerializer
@@ -10,14 +10,14 @@ from post.models import Post
 class AdminPostListView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
-    permission_classes = [IsAdminUser, ]
+    permission_classes = (IsAdminUser,)
     lookup_field = 'slug'
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = (JSONWebTokenAuthentication,)
 
 
 class AdminPostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = AdminPostDetailSerializer
-    permission_classes = [IsAdminUser, ]
-    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminUser,)
+    authentication_classes = (JSONWebTokenAuthentication,)
     lookup_field = 'slug'
--- backend/blog_backend/api/admin/user/views.py
+++ backend/blog_backend/api/admin/user/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from user_profile.models import UserProfile
 from .serializers import AdminUserDetailSerializer, AdminUserListSerializer
@@ -12,10 +13,12 @@ User = get_user_model()
 class AdminUserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = AdminUserListSerializer
-    permission_classes = [IsAdminUser, ]
+    permission_classes = (IsAdminUser,)
+    authentication_classes = (JSONWebTokenAuthentication,)
 
 
 class AdminUserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = AdminUserDetailSerializer
-    permission_classes = [IsAdminUser,]
+    permission_classes = (IsAdminUser,)
+    authentication_classes = (JSONWebTokenAuthentication,)
