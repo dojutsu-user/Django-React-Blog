ac132bd8976c3877b70e050ae2e3f435de88e5d3
dojutsu-user <vaibhgupt199@gmail.com>
2018-07-28 11:41:30 +0530

    Add "Danger" Button Type To Button UI Component

--- frontend/blog_frontend/src/components/UI/Button/Button.js
+++ frontend/blog_frontend/src/components/UI/Button/Button.js
@@ -3,8 +3,14 @@ import React from "react";
 import cssClass from "./Button.css";
 
 const button = props => {
+    let css = null;
+    if (props.red) {
+        css = cssClass.Danger;
+    } else {
+        css = cssClass.Button;
+    }
     return (
-        <button disabled={props.disabled} className={cssClass.Button}>
+        <button disabled={props.disabled} className={css}>
             {props.children}
         </button>
     );
