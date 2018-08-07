4043a11b5c9513ee1e93539eada9ed3164472cb3
dojutsu-user <vaibhgupt199@gmail.com>
2018-08-07 12:48:09 +0530

    Remove extra CSS Classes Assignment Function

--- frontend/blog_frontend/src/components/UI/NavigationItems/NavigationItem/NavigationItem.js
+++ frontend/blog_frontend/src/components/UI/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,13 +3,7 @@ import React from "react";
 import cssClass from "./NavigationItem.css";
 
 const navigationItem = props => {
-
-    let classes = [cssClass.Item,]
-    if (props.active) {
-        classes.push(cssClass.active)
-    }
-
-    return <li className={classes.join(' ')}>{props.children}</li>;
+    return <li className={cssClass.Item}>{props.children}</li>;
 };
 
 export default navigationItem;
