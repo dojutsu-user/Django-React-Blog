f535484c128859c8005625a0ed4165a6a2e51df5
dojutsu-user <vaibhgupt199@gmail.com>
2018-08-08 23:56:25 +0530

    Show 'No Posts yet' if there are no posts.

--- frontend/blog_frontend/src/components/Posts/Posts.js
+++ frontend/blog_frontend/src/components/Posts/Posts.js
@@ -1,19 +1,27 @@
 import React from "react";
 
 import Post from "./Post/Post";
+import cssClass from "./Posts.css";
 
 const posts = props => {
-    const postList = props.postList.map(post => (
-        <Post
-            key={post.slug}
-            slug={post.slug}
-            title={post.title}
-            datePublished={post.published_on}
-            totalComments={post.total_comments}
-            author={post.author_full_name}
-            short_description={post.short_description}
-        />
-    ));
+    let postList = null
+    if (props.postList.length > 0) {
+        postList = props.postList.map(post => (
+            <Post
+                key={post.slug}
+                slug={post.slug}
+                title={post.title}
+                datePublished={post.published_on}
+                totalComments={post.total_comments}
+                author={post.author_full_name}
+                short_description={post.short_description}
+            />
+        ));
+    } else {
+        postList = (
+            <div className={cssClass.NoPosts}>No Posts Yet</div>
+        )
+    }
     return <div>{postList}</div>;
 };
 
