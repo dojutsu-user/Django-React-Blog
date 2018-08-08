436ec3629b16e472ca80c9730c05f5ddb1d8efe5
dojutsu-user <vaibhgupt199@gmail.com>
2018-08-09 00:04:34 +0530

    Add URL to show the documentation of the API.

--- backend/blog_backend/blog_backend/urls.py
+++ backend/blog_backend/blog_backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('auth/registration/', include('rest_auth.registration.urls')),
     path('dashboard/', include('api.dashboard.urls')),
     path('comments/', include('api.comment.urls')),
+    path('docs/', include_docs_urls(title='Blog API Documentation'))
 ]
