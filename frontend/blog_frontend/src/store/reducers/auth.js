fccca9d38db29fc925fde58aa1b609362be5b654
dojutsu-user <vaibhgupt199@gmail.com>
2018-08-09 23:06:30 +0530

    Change The Directory Of 'utility.js'

--- frontend/blog_frontend/src/store/reducers/auth.js
+++ frontend/blog_frontend/src/store/reducers/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
-import { updateObject } from "../utility";
+import { updateObject } from "../../shared/utility";
 
 const initialState = {
     token: null,
